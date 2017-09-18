c36fc91b8cd172c49383df34b2ca995241c33be3
Carlos Gutierrez <rapsta93@gmail.com>
2017-09-18 10:16:00 -0500

    add modelo de colegios y usuarios

--- eees/admin.py
+++ eees/admin.py
@@ -9,3 +9,4 @@ from .models import *
 
 admin.site.register(Departamento)
 admin.site.register(Municipio)
+admin.site.register(Colegio)
--- eees/models.py
+++ eees/models.py
@@ -28,4 +28,61 @@ class Municipio(models.Model):
     municipio       = models.CharField(max_length=200)
 
     def __str__(self):
-        return self.municipio
+        return str(self.departamento) +str(' - ')+ str(self.municipio)
+
+#Modelo de Colegio
+class Colegio(models.Model):
+    class Meta:
+        verbose_name = "Colegio"
+        verbose_name_plural = "Colegios"
+
+    nombre          = models.CharField(max_length=200)
+    departamento    = models.ForeignKey(Departamento, on_delete=models.CASCADE)
+    municipio       = models.ForeignKey(Municipio, on_delete=models.CASCADE)
+    is_active       = models.BooleanField('Activar', default=True)
+
+    def __str__(self):
+        return self.nombre
+
+    def desactivar(self):
+        self.is_active = False
+
+    def activar(self):
+        self.is_active = True
+
+#modelo de usuarios
+class Usuario(AbstractUser):
+    class Meta:
+        db_table = 'auth_user'
+        verbose_name = "Usuario"
+        verbose_name_plural = "Usuarios"
+
+    TIPO_DOC_LIST = [
+        ('CEDULA CUIDADANIA', 'CEDULA CUIDADANIA'),
+        ('CEDULA EXTRANJERIA', 'CEDULA EXTRANJERIA'),
+        ('PASAPORTE', 'PASAPORTE'),
+        ('TARJETA IDENTIDAD', 'TARJETA IDENTIDAD'),
+    ]
+    GENERO_LIST= [
+        ('HETEROSEXUAL', 'HETEROSEXUAL'),
+        ('HOMOSEXUAL', 'HOMOSEXUAL'),
+        ('LESBIANA', 'LESBIANA'),
+        ('BISEXUAL', 'BISEXUAL'),
+        ('INDIFERENCIADO', 'INDIFERENCIADO'),
+    ]
+
+    is_admin            = models.BooleanField('Administrador',default=False)
+    is_profe            = models.BooleanField('Profesor',default=False)
+    is_estud            = models.BooleanField('Estudiante',default=False)
+    tipo_docto          = models.CharField('Tipo documento',max_length=20,choices=TIPO_DOC_LIST)
+    no_docto            = models.CharField('Numero documento',max_length=20)
+    fecha_nac           = models.DateField('Fecha nacimiento',null=True)
+    genero              = models.CharField('Genero',max_length=20,choices=GENERO_LIST)
+    direccion           = models.CharField('Direccion',max_length=100)
+    telefono            = models.CharField('Telefono',max_length=15)
+
+    def desactivar(self):
+        self.is_active = False
+
+    def activar(self):
+        self.is_active = True
